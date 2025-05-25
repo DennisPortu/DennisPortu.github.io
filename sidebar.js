7fb876928055a290689a2517ece697293e9aed9f
Evil159T <pausanchez0305@gmail.com>
2025-05-26 01:22:08 +0200

    SidebarCLosed

--- sidebar.js
+++ sidebar.js
@@ -2,9 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const sidebar = document.querySelector(".Sidebar");
   const toggleBtn = document.getElementById("toggleSidebar");
 
-  if (localStorage.getItem("sidebar") === "hidden") {
-    sidebar.style.transform = "translateX(-100%)";
-  }
+// Forzar que empiece cerrada sin mirar localStorage
+sidebar.style.transform = "translateX(-100%)";
+localStorage.setItem("sidebar", "hidden");
+
 
   toggleBtn.addEventListener("click", () => {
     const isHidden = sidebar.style.transform === "translateX(-100%)";
